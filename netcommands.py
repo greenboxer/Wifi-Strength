c484cbb99be2b3a1557ae958772e1d5315b5f7e1
greenboxer <richardchoi.tt@gmail.com>
2024-04-13 12:50:48 -0700

    Update netcommands.py
    modify the main getnetsh() to getnetstr() and added an OS switch to call either netsh ir iwconfig

--- netcommands.py
+++ netcommands.py
@@ -1,28 +1,51 @@
 import subprocess
 import re
 import sys
+import os
 
 
 class netcommands():
 
-    def getnetsh(self):
-        try:
-            result = subprocess.check_output(['netsh','wlan','show','interfaces']).decode(sys.stdout.encoding)
-        except subprocess.CalledProcessError as err:
-            print('netsh Command error' + err)
-            exit(1)
-        ssid,strength = self.findstrength(result)
+    def getnetstr(self):
+        if os.name == 'nt':
+            try:
+                result = subprocess.check_output(['netsh','wlan','show','interfaces']).decode(sys.stdout.encoding)
+            except subprocess.CalledProcessError as err:
+                print('netsh Command error' + err)
+                exit(1)
+            ssid,strength = self.parsenetsh(result)
+        elif os.name == 'posix':
+            try:
+                result = subprocess.check_output(['iwconfig']).decode(sys.stdout.encoding)
+            except subprocess.CalledProcessError as err:
+                print('iwconfig Command error ' + err)
+                exit(1)
+            ssid,strength = self.parseiwconfig(result)
+        else:
+            ssid = 'error'
+            strength = '0'
+
         return ssid,strength
 
-    def findstrength(self,string):
-        match1 = re.search("Signal.+: (\d{1,3})%",string)
+    def parsenetsh(self,string):
+        match1 = re.search('Signal.+: (\d{1,3}%)',string)
         strength = match1.group(1)
 
-        match2 = re.search("SSID.+: (.+)",string)
+        match2 = re.search('SSID.+: (.+)',string)
+        ssid = match2.group(1)
+
+        return ssid,strength
+
+    def parseiwconfig(self,string):
+        match1 = re.search('Signal level=(-\d{1,3} dBm)',string)
+        strength = match1.group(1)
+
+        match2 = re.search('SSID:\"(.+)\"',string)
         ssid = match2.group(1)
 
         return ssid,strength
 
 if __name__ == "__main__":
     nc = netcommands()
-    ssid,strength = nc.getnetsh()
+    ssid,strength = nc.getnetstr()
+    print('SSID: ' + ssid + ', Strength: ' + strength)
